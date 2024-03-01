605590ca593aa531d6224112722783a955f31666
Karol Bautrel <karol.bautrel@gmail.com>
2024-03-02 00:01:14 +0100

    Set db connection with gorm

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,10 +1,27 @@
-package db
+package main
 
 import (
 	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func DBConnector() string {
-	fmt.Println("DB")
-	return "DB"
+func DBConnector() (*gorm.DB, error) {
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	dbName := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASSWORD")
+	sslMode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=%s password=%s", host, user, dbName, sslMode, password)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+	fmt.Println("Connected to DB")
+
+	return db, nil
 }
