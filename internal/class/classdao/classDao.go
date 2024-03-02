5d2911ce3efc79b7d34b9d5e2e979a594d733ded
Karol Bautrel <karol.bautrel@gmail.com>
2024-03-03 00:02:02 +0100

    Created Class Domain Repository\

--- internal/class/classdao/classDao.go
+++ internal/class/classdao/classDao.go
@@ -1,4 +1,14 @@
 package classdao
 
+import (
+	"virtual-diary/internal/student/studentdao"
+
+	"gorm.io/gorm"
+)
+
 type Class struct {
+	gorm.Model
+	Name     string
+	Profile  string
+	Students []studentdao.Student
 }
--- internal/class/classrepo.go
+++ internal/class/classrepo.go
@@ -1,8 +1,64 @@
 package class
 
+import (
+	"virtual-diary/internal/class/classdao"
+
+	"gorm.io/gorm"
+)
+
 type classRepoImpl struct {
+	db *gorm.DB
 }
 
-func NewClassRepository() ClassRepository {
-	return &classRepoImpl{}
+func NewClassRepository(db *gorm.DB) ClassRepository {
+	return &classRepoImpl{db: db}
+}
+
+func (r *classRepoImpl) GetAllClasses() ([]classdao.Class, error) {
+	var classes []classdao.Class
+	err := r.db.Find(&classes).Error
+	if err != nil {
+		return nil, err
+	}
+	return classes, nil
+}
+
+func (r *classRepoImpl) GetClassById(id string) (classdao.Class, error) {
+	class := classdao.Class{}
+	result := r.db.Where("id = ?", id).First(&class)
+	if result.Error != nil {
+		return classdao.Class{}, result.Error
+	}
+	return class, nil
+}
+
+func (r *classRepoImpl) CreateClass(name string, profile string) (bool, error) {
+	newClass := classdao.Class{
+		Name:    name,
+		Profile: profile,
+	}
+	result := r.db.Create(&newClass)
+	if result.Error != nil {
+		return false, result.Error
+	}
+	return true, nil
+}
+
+func (r *classRepoImpl) RemoveClass(classId string) (bool, error) {
+	class, err := r.GetClassById(classId)
+	if err != nil {
+		return false, err
+	}
+
+	if result := r.db.Delete(&class); result.Error != nil {
+		return false, result.Error
+	}
+
+	return true, nil
+
+}
+
+func (r *classRepoImpl) RemoveStudentFromClass(studentId string, classId string) string {
+
+	return "will be after student dao"
 }
