ab3279d4238290b5e57b76754d44be0d64f7f314
Karol Bautrel <karol.bautrel@gmail.com>
2024-03-19 00:14:52 +0100

    Added beginning of write service

--- internal/student/studentwriteservice.go
+++ internal/student/studentwriteservice.go
@@ -1,9 +1,28 @@
 package student
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type studentWriteServiceImpl struct {
 	repository StudentRepo
 }
 
 func NewWriteStudentService(repo StudentRepo) StudentWriteService {
 	return &studentWriteServiceImpl{repository: repo}
+}
+
+func (s *studentWriteServiceImpl) CreateStudent(name, surname, age, classId string) (bool, error) {
+	intAge, err := strconv.Atoi(age)
+	if err != nil {
+		return false, fmt.Errorf("conversion to int failed: %w", err)
+	}
+
+	intId, err := strconv.Atoi(classId)
+	if err != nil {
+		return false, fmt.Errorf("conversion to int failed: %w", err)
+	}
+
+	return s.repository.CreateStudent(name, surname, intAge, uint(intId))
 }
