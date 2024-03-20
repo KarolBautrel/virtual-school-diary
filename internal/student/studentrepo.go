14d98410d233b7c47382ba2170e84de786bbf378
Karol Bautrel <karol.bautrel@gmail.com>
2024-03-21 00:05:29 +0100

    Student Repo{

--- internal/student/studentrepo.go
+++ internal/student/studentrepo.go
@@ -1,6 +1,8 @@
 package student
 
 import (
+	"virtual-diary/internal/student/studentdao"
+
 	"gorm.io/gorm"
 )
 
@@ -10,4 +12,49 @@ type studentRepoImpl struct {
 
 func NewStudentRepo(db *gorm.DB) StudentRepo {
 	return &studentRepoImpl{db: db}
+}
+
+func (r *studentRepoImpl) GetStudentById(id string) (studentdao.Student, error) {
+	student := studentdao.Student{}
+	result := r.db.Where("id = ?", id).First(&student)
+	if result.Error != nil {
+		return studentdao.Student{}, result.Error
+	}
+
+	return student, nil
+}
+func (r *studentRepoImpl) GetStudentsByClass(classId string) ([]studentdao.Student, error) {
+	students := []studentdao.Student{}
+	err := r.db.Find(&students).Error
+	if err != nil {
+		return []studentdao.Student{}, err
+	}
+	return students, nil
+}
+
+func (r *studentRepoImpl) CreateStudent(name string, surname string, age int, classId uint) (bool, error) {
+
+	newStudent := studentdao.Student{
+		Name:    name,
+		Surname: surname,
+		Age:     age,
+		ClassID: classId,
+	}
+	if result := r.db.Create(&newStudent); result.Error != nil {
+		return false, result.Error
+	}
+	return true, nil
+
+}
+
+func (r *studentRepoImpl) DeleteStudent(studentId string) (bool, error) {
+	student, err := r.GetStudentById(studentId)
+	if err != nil {
+		return false, err
+	}
+	if result := r.db.Delete(&student); result.Error != nil {
+		return false, result.Error
+	}
+	return true, nil
+
 }
