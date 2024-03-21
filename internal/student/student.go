f7708853ad0581cee2ee7891efd6bb6784283180
Karol Bautrel <karol.bautrel@gmail.com>
2024-03-22 00:14:15 +0100

    Added interface to student repository

--- internal/student/student.go
+++ internal/student/student.go
@@ -1,10 +1,10 @@
 package student
 
-type StudentReadService interface {
-}
+import "virtual-diary/internal/student/studentdao"
 
 type StudentRepo interface {
-}
-
-type StudentWriteService interface {
+	GetStudentById(id string) (studentdao.Student, error)
+	GetStudentsByClass(className string) ([]studentdao.Student, error)
+	CreateStudent(name string, surname string, age int, classId uint) (bool, error)
+	DeleteStudent(studentId string) (bool, error)
 }
