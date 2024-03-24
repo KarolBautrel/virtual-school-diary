c90565bb0783429d0aaef7b5c5c2c390b9cdb50b
Karol Bautrel <karol.bautrel@gmail.com>
2024-03-24 01:56:49 +0100

    Added utils to class to conver dao to dto

---
+++ internal/class/utils.go
@@ -0,0 +1,11 @@
+package class
+
+import (
+	"virtual-diary/internal/class/classdao"
+	"virtual-diary/internal/class/classdto"
+)
+
+func ConvertDaoToDto(classDTO *classdto.ClassDto, classDAO *classdao.Class) {
+	classDTO.Name = classDAO.Name
+	classDTO.Profile = classDAO.Profile
+}
