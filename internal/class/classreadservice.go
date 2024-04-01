d487dcbde34900ba8d573b6af27f01873a738201
Karol Bautrel <karol.bautrel@gmail.com>
2024-04-02 01:13:17 +0200

    Restructured errors

--- internal/class/classreadservice.go
+++ internal/class/classreadservice.go
@@ -1,6 +1,7 @@
 package class
 
 import (
+	"fmt"
 	"time"
 	"virtual-diary/internal/class/classdto"
 	gloablutils "virtual-diary/pkg/utils"
@@ -19,7 +20,7 @@ func (s *ClassReadService) GetAllClasses() ([]classdto.ClassDto, error) {
 	defer cancel()
 	classes, err := s.repository.GetAllClasses(ctx)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error in read service: %s", err)
 	}
 	classesDTO := []classdto.ClassDto{}
 
@@ -38,7 +39,8 @@ func (s *ClassReadService) GetClassById(id string) (classdto.ClassDto, error) {
 	class, err := s.repository.GetClassById(id, ctx)
 	var classDTO classdto.ClassDto
 	if err != nil {
-		return classdto.ClassDto{}, err
+		return classdto.ClassDto{}, fmt.Errorf("error in read service: %s", err)
+
 	}
 	ConvertClassDaoToDto(&classDTO, &class)
 
