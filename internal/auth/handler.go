d456eb14180d6b2e5d38c5255d6c91e9e9940895
Karol Bautrel <karol.bautrel@gmail.com>
2024-04-06 00:49:14 +0200

    added new method to handler

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -28,13 +28,13 @@ func RegisterRoutes(router *mux.Router, readService *AuthReadService) {
 func SignIn(w http.ResponseWriter, r *http.Request, readService *AuthReadService) {
 	var siginInInput SignInInput
 	if err := json.NewDecoder(r.Body).Decode(&siginInInput); err != nil {
-		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		http.Error(w, "invalid request payload", http.StatusBadRequest)
 		return
 	}
 	token, err := readService.SignIn(siginInInput.Username, siginInInput.Password)
 
 	if err != nil {
-		http.Error(w, "Something went wrong", http.StatusBadRequest)
+		http.Error(w, "something went wrong", http.StatusBadRequest)
 		return
 
 	}
@@ -48,7 +48,18 @@ func GetUserByUsername(w http.ResponseWriter, r *http.Request, readService *Auth
 	username := vars["username"]
 	user, err := readService.GetUserByUsername(username)
 	if err != nil {
-		http.Error(w, "Something went wrong", http.StatusBadRequest)
+		http.Error(w, "something went wrong wit getting user by username", http.StatusBadRequest)
+		return
+
+	}
+	fmt.Fprintln(w, user)
+}
+func GetUserById(w http.ResponseWriter, r *http.Request, readService *AuthReadService) {
+	vars := mux.Vars(r)
+	userId := vars["id"]
+	user, err := readService.GetUserById(userId)
+	if err != nil {
+		http.Error(w, "something went wrong wit getting user by id", http.StatusBadRequest)
 		return
 
 	}
