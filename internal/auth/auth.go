e0bc32f7b10e3847a649dcbe1e8da231524aca94
Karol Bautrel <karol.bautrel@gmail.com>
2024-04-09 01:15:20 +0200

    aded interface

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -8,4 +8,5 @@ import (
 type AuthRepository interface {
 	CreateUser(username string, email string, password string) (bool, error)
 	GetUserByUsername(username string, ctx context.Context) (userdao.User, error)
+	GetUserById(userId string, ctx context.Context) (userdao.User, error)
 }
