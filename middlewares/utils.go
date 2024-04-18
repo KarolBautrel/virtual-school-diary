8b3ab8220e115799fd7aee2bf23fbd1f527387aa
Karol Bautrel <karol.bautrel@gmail.com>
2024-04-19 00:23:55 +0200

    Added validation for secret key

--- middlewares/utils.go
+++ middlewares/utils.go
@@ -3,17 +3,12 @@ package middlewares
 import (
 	"errors"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
-//	func keyFunc(token *jwt.Token) (interface{}, error) {
-//		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-//			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-//		}
-//		return []byte("ThereWillBeKey"), nil
-//	}
 func VerifyToken(tokenString string) error {
 	tokenString = strings.TrimPrefix(tokenString, "Bearer ")
 
@@ -21,7 +16,11 @@ func VerifyToken(tokenString string) error {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
-		return []byte("ThereWillBeKey"), nil
+		secret := os.Getenv("SECRET_KEY")
+		if secret == "" {
+			return nil, errors.New("secret key is not set")
+		}
+		return []byte(secret), nil
 	})
 
 	if err != nil {
