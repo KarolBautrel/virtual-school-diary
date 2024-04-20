773cc974177820625452077dbf24a82c9de2fcdb
Karol Bautrel <karol.bautrel@gmail.com>
2024-04-20 21:04:56 +0200

    Adjusted midddleware of jwt to response invalid jwt verification

--- middlewares/jwtmiddleware.go
+++ middlewares/jwtmiddleware.go
@@ -1,7 +1,6 @@
 package middlewares
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -9,7 +8,8 @@ func JWTmiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		token := r.Header.Get("Authorization")
 		if err := VerifyToken(token); err != nil {
-			fmt.Errorf("jwt verify error")
+			http.Error(w, "jwt verify error", http.StatusUnauthorized)
+			return
 		}
 		next.ServeHTTP(w, r)
 	}
